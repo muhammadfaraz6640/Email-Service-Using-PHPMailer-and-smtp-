ea0b9b4fe8a75b2ba0a83d902819cd5cdf388cd3
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-05-16 17:29:55 +0500

    Emails Files.I will Modified it later in this Repo

---
+++ Email/index.php
@@ -0,0 +1,37 @@
+<?php
+require_once('PHPMailerAutoload.php');
+require_once('class.phpmailer.php');
+require_once('class.smtp.php');
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'appartmentmanagement200@gmail.com';                 // SMTP username
+$mail->Password = 'karachi94';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('appartmentmanagement200@gmail.com', 'Mailer');
+$mail->addAddress('muhammad.faraz9@yahoo.com');     // Add a recipient        
+$mail->addReplyTo('appartmentmanagement200@gmail.com');
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com');
+
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
